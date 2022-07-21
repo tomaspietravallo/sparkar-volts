1a5e8421cd9a115c02b6644d835c184df9231b8b
Tomás Pietravallo <pietravallo21tomas@gmail.com>
2022-07-21 19:28:03 -0300

    O(n^2) collision checks. needs optimization

--- volts.ts
+++ volts.ts
@@ -2270,6 +2270,7 @@ export class Object3D<T extends SceneObjectBase = any> {
       group?: number;
     } = DefaultPhysicsSettings,
   ): Object3D {
+    if (args == undefined) args = {};
     Object3D.createPhysicsSolver(this, args);
     (Object3D.colliders[args.group] = Object3D.colliders[args.group] || []).push(this);
     return this;
@@ -2478,33 +2479,40 @@ export class Object3D<T extends SceneObjectBase = any> {
           i++;
         }
 
-        const objA = group[0];
-        const objB = group[1];
-
-        const obbA = new OBB({
-          position: objA.pos.copy(),
-          size: new Vector(0.05),
-          orientation: objA.rot.toMatrix().inverse(),
-        });
-        
-        const obbB = new OBB({
-          position: objB.pos.copy(),
-          size: new Vector(0.05),
-          orientation: objB.rot.toMatrix().inverse(),
-        });
-
-        const collision = obbA.againstOBB(obbB);
-
-        if (collision) {
-          const { overlap, axis } = collision as { overlap: number; axis: Vector<3> };
-          // overlap += 0.001;
-
-          objA.pos.add(axis.copy().mul(objA.pos.copy().normalize().dot(axis) * overlap * 5));
-          objB.pos.add(axis.copy().mul(objB.pos.copy().normalize().dot(axis) * overlap * 5));
-
-          const poc = obbA.closestToPoint(objB.pos).add(obbB.closestToPoint(objA.pos)).div(2);
-          Object3D.solveCollision(objA, objB, poc);
+        // @todo O(n^2) - needs optimizing 
+        for (let j = 0; j < group.length; j++) {
+          for (let k = 0; k < group.length; k++) {
+            if (j === k) continue
+            const objA = group[j];
+            const objB = group[k];
+    
+            const obbA = new OBB({
+              position: objA.pos.copy(),
+              size: new Vector(0.05),
+              orientation: objA.rot.toMatrix().inverse(),
+            });
+            
+            const obbB = new OBB({
+              position: objB.pos.copy(),
+              size: new Vector(0.05),
+              orientation: objB.rot.toMatrix().inverse(),
+            });
+    
+            const collision = obbA.againstOBB(obbB);
+    
+            if (collision) {
+              const { overlap, axis } = collision as { overlap: number; axis: Vector<3> };
+              // overlap += 0.001;
+    
+              objA.pos.add(axis.copy().mul(objA.pos.copy().normalize().dot(axis) * overlap * 5));
+              objB.pos.add(axis.copy().mul(objB.pos.copy().normalize().dot(axis) * overlap * 5));
+    
+              const poc = obbA.closestToPoint(objB.pos).add(obbB.closestToPoint(objA.pos)).div(2);
+              Object3D.solveCollision(objA, objB, poc);
+            }
+          }
         }
+
       }
       i++;
     }
